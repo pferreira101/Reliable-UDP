d25e32a086e8543ff20259fca93790cf3262f896
Pedro Moreira <pedrombm19@gmail.com>
2019-04-08 18:41:37 +0100

    Divide file

--- src/AgenteUDP.java
+++ src/AgenteUDP.java
@@ -1,2 +1,33 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class AgenteUDP {
+
+
+    /*
+        - Passar logo file em vez do path?
+        - Retornar uma lista de Packets já montados?
+     */
+    public void dividePacket(String path, int max) throws IOException {
+        File file = new File(path);
+
+        byte[] content = Files.readAllBytes(file.toPath());
+
+        int frag = content.length/max;
+        System.out.println("Frags = " + frag);
+
+        ArrayList<byte[]> fragmentos = new ArrayList<>();
+        for(int i = 0; i<frag; i++){
+
+            fragmentos.add(Arrays.copyOfRange(content,i*max, i*max+max-1));
+
+            System.out.println("Fragmento " + i + " completo.");
+        }
+
+        fragmentos.add(Arrays.copyOfRange(content,frag*max-1, content.length-1));
+
+    }
 }
---
+++ src/TransfereCC.java
@@ -0,0 +1,24 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TransfereCC {
+
+
+    public static void main(String args[]) throws UnknownHostException {
+        if(args.length == 1){
+            try{
+                System.out.println(InetAddress.getByName(args[0]).toString());
+                System.out.println("Válido\n");
+            }
+            catch (Exception e){
+                System.out.println("Inválido\n");
+            }
+        }
+        else{
+            System.out.println("Introduza um IP \n");
+        }
+    }
+}
